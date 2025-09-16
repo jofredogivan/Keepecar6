1f0142f0e2f22e9b153b279f00d4b2a6b4155a63
jofredogivan <jofredogivan@gmail.com>
2025-09-16 17:58:44 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "keepecar6-cache-v2"; // Versão do cache atualizada
+const CACHE_NAME = "keepecar6-cache-v2.1"; // Versão do cache atualizada
 const FILES_TO_CACHE = [
   "./index.html",
   "./manifest.json",
@@ -49,5 +49,6 @@ self.addEventListener("fetch", (evt) => {
     })
   );
 });
+
 
 
